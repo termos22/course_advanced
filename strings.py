0de5da1b85e469e8c3908e7d4e3e698a81590b48
Jakub Przybysz <termos22@gmail.com>
2024-02-18 20:25:25 +0100

    add string code from course

---
+++ strings.py
@@ -0,0 +1,10 @@
+ex_1 = 'This is a string!'
+ex_2 = "This is also a string"
+ex_3 = '1984!'
+ex_4 = "LiVe LONG and PRosPEr."
+ex_5 = "!@#$%^&*()_-=+=/."
+ex_6 = ""
+ex_7 = "0123456"
+ex_8 = "orange"
+print(ex_8[2])
+print("apple"[4])
