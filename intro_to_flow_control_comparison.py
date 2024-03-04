26e5f2a7da492c42de71801aa023e566dd777ef6
Jakub Przybysz <termos22@gmail.com>
2024-03-04 23:40:03 +0100

    introduction to comparisong numbers, strings and boolean

---
+++ intro_to_flow_control_comparison.py
@@ -0,0 +1,21 @@
+# comparison operators - operatory porównania
+# 
+#   >   - większe
+#   <   - mniejsze
+#   >=  - większe równe
+#   <=  - mniejsze równe
+#   !=  - nie równe
+#   ==  - równe
+
+
+print(4 > 2)
+print(1 > 3)
+
+print(5.79 < 6)
+print(3 < 3)
+
+print(9 >= 9)
+print(1 <= 2)
+
+print(10 != 100)
+print(10 != 10)
