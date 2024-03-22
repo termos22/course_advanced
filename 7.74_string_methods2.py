c1e9bbeb620e67a5fbd9200cc10b073298a1c8ff
Jakub Przybysz <termos22@gmail.com>
2024-03-22 20:04:00 +0100

    cleaning file names

--- 7.74_string_methods2.py
+++ 7.74_string_methods2.py
@@ -18,3 +18,15 @@ print("hello world".center(15, "*"))
 # .rstrip() - domyślnie usuwa ze stringu wszystkie spacje znajdujące się z prawej strony
 # .lstrip() - domyślnie usuwa ze stringu wszystkie spacje znajdujące się z lewej strony
 # jako agument do powyższych metod można przesłać string który chcemy usunąć z głownego stringu
+print("I had an exciting trip!!!11111")
+print("I had an exciting trip!!!11111".strip("1"))
+print("I had an exciting trip!!!11111".rstrip("1"))
+print("I had an exciting trip!!!11111".lstrip("1"))
+
+print("juice, bread, cheese, beef, bread".rstrip(", bread"))
+print("juice, bread, cheese, beef, bread".rstrip(",ed arb")) # metoda rozpoznaje znaki więc nie jest ważna ich kolejność
+
+print("blueblueyellowblue".strip("eulb"))
+
+# .replace() - wyszukuje i zamienia string
+print("Good morning.".replace("morning", "afternoon"))
