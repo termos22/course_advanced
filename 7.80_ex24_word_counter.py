3e5c701cec90089ee7947acb5269ae5a9e789add
Jakub Przybysz <termos22@gmail.com>
2024-03-22 22:08:53 +0100

    started exercise 24 word counter

---
+++ 7.80_ex24_word_counter.py
@@ -0,0 +1,8 @@
+str_1 = "James Bond is 007."
+str_2 = "When the moon hits your eye like a big pizza pie, that's amore!"
+str_3 = "Anyway, like I was sayin', shrimp is the fruit of the sea. You can barbecue it, boil it, broil it, bake it, \
+saute it. Dey's uh, shrimp-kabobs, shrimp creole, shrimp gumbo. Pan fried, deep fried, stir-fried. There's pineapple \
+shrimp, lemon shrimp, coconut shrimp, pepper shrimp, shrimp soup, shrimp stew, shrimp salad, shrimp and potatoes, \
+shrimp burger, shrimp sandwich. That- that's about it."
+
+
